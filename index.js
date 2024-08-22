6833c57192d8bb3f71aa650aa057643f2a825529
rikka-maj123 <anusha.majumdar@appinventiv.com>
2024-08-22 13:31:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer((req, res) => {
     // Set the response HTTP header with HTTP status and content type
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     // Send the response body "Hello World!"
-    res.end('Hello ...Anusha ....World!\n');
+    res.end('Hello ...Anusha ....\n');
 });
 
 // Make the server listen on the specified port
